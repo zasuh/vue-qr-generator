65beb63ea67ca22317b25407948c3de6aadb6d43
Zane Suhadolnik <zanesuhi@gmail.com>
2020-06-22 06:43:39 +0200

    Updated

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # vue-qr-generator
 
+Generate a QR code from the entered URL.
+
 ## Project setup
 ```
 npm install
@@ -8,14 +10,4 @@ npm install
 ### Compiles and hot-reloads for development
 ```
 npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
 ```
---
+++ src/components/Main.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <input type="text" v-model="url">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Main',
+    props: {
+      msg: String
+    },
+    data () {
+      return {
+        url: ''
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
