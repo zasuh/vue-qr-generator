d8e7cbc910f08cbc45b2d866a083f58644083889
Zane Suhadolnik <zanesuhi@gmail.com>
2020-06-24 06:58:27 +0200

    Updated

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,6 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <input type="text" v-model="url">
+    <input type="button" value="Convert" v-on:click="convert(url)">
+    <img :src="url" alt="The QR code">
   </div>
 </template>
 
@@ -14,6 +16,12 @@
     data () {
       return {
         url: ''
+      }
+    },
+    methods: {
+      convert: function(url) {
+        const QR_URL = `https://qrtag.net/api/qr(_transparent)(_100x100).png(?url=${url})`;
+        this.url = QR_URL;
       }
     }
   }
